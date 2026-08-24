17503559029475263ca86ad73e3162bd2ff01896
Pinku Nath <pinkunath782486@gmail.com>
2026-08-24 18:50:12 +0530

    Add Student and Department Entities

---
+++ src/main/java/com/pinkunath/collegemanagement/entity/Department.java
@@ -0,0 +1,22 @@
+package com.pinkunath.collegemanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "department")
+@Getter
+@Setter
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Student> students;
+}
---
+++ src/main/java/com/pinkunath/collegemanagement/entity/Student.java
@@ -0,0 +1,25 @@
+package com.pinkunath.collegemanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int rollNo;
+    private String name;
+    private int deptID;
+    private String contactNo;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "deptId")
+    private Department department;
+}
